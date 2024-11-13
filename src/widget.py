876daff5f33bfd6ee0e3a9c177aad1458c9b360a
Ildan Ibatullin <rustam162606@yandex.ru>
2024-11-13 11:30:52 +0300

    Третий коммит: добавление функции get_date

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,26 @@
+# src/widget.py
+
 def mask_account_card(info):
-    card_types = ["Visa", "Maestro", "MasterCard"]
-    # Проверяем тип карты или счета
-    for card_type in card_types:
-        if info.startswith(card_type):
-            num = info.split()[-1]  # получаем номер карты
-            return f"{' '.join(info.split()[:-1])} {num[:4]} {num[4:6]}** **** {num[-4:]}"
+    # Разделяем строку на тип и номер
+    parts = info.rsplit(' ', 1)
+    card_type = parts[0]
+    number = parts[1]
 
-    # Если это счет
-    if info.startswith("Счет"):
-        num = info.split()[-1]
-        return f"Счет **{num[-4:]}"
+    if "Счет" in card_type:
+        # Маскировка для счета
+        masked_number = f"{card_type} **{number[-4:]}"
+    else:
+        # Маскировка для карт
+        masked_number = f"{card_type} {number[:4]} {number[4:6]}** **** {number[-4:]}"
 
-    return "Неизвестный тип информации"
+    return masked_number
+
+
+# src/widget.py
+
+def get_date(date_str):
+    # Разделяем строку по символу 'T'
+    date_part = date_str.split('T')[0]
+    year, month, day = date_part.split('-')
+    return f"{day}.{month}.{year}"
+
